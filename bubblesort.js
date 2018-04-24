2270c9c434eae6212005c701d2bfc5fa2896cbfc
Linda Morales <lindagmorales94@gmail.com>
2018-04-23 21:55:57 -0400

    It works

--- bubblesort.js
+++ bubblesort.js
@@ -2,18 +2,23 @@ function bubbleSort(array) {
   if (array.length <= 1) {
     return array;
   } else {
-    if (compare(array[0], array[1])) {
-      swap.call(array, 0);
+    // if (compare(array[0], array[1])) {
+    //   swap.call(array, 0);
+    // }
+    // array = array.slice(0, 1).concat(bubbleSort(array.slice(1)));
+    for (let i = 0; i < array.length - 1; i++) {
+      if (compare(array[i], array[i + 1])) {
+        swap(array, i);
+      }
     }
-    array = array.slice(0, 1).concat(bubbleSort(array.slice(1)));
     return bubbleSort(array.slice(0, -1)).concat(array[array.length - 1]);
   }
 }
 
-function swap(index) {
-  let temp = this[index];
-  this[index] = this[index + 1];
-  this[index + 1] = temp;
+function swap(arr, index) {
+  let temp = arr[index];
+  arr[index] = arr[index + 1];
+  arr[index + 1] = temp;
 }
 
 function compare(num1, num2) {
